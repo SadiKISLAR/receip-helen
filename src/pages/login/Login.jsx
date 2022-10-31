51a300634fc3958b2a955000278d538ac9f0216c
SadiKISLAR <sadi22kislar@gmail.com>
2022-10-31 21:53:00 +0300

    styled

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import LoginContainer from './Login.style'
-
+import meal from "../../assets/meal.svg"
 const Login = () => {
   return (
     <LoginContainer>
-      
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>
+          {"<CW"}Recipe
+        </Header>
+        <StyledForm>
+          <StyledInput>
+          </StyledInput>
+          <StyledButton>Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+
     </LoginContainer>
   )
 }
