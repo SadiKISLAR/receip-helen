72bd2b79021856edefc7bcf6923352e1c160aecc
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-01 09:35:09 +0300

    approuter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { GlobalStyles } from '../components/globalStyles/GlobalStyles'
+import Navbar from "./../components/nav/Navbar"
+import Home from "../pages/home/Home"
 
 const AppRouter = () => {
   return (
-    <div>
-      App Router
-    </div>
+    <BrowserRouter>
+      <GlobalStyles />
+      <Navbar />
+      <Routes>
+        <Route path="/" elemnet={<Home/>} />
+      </Routes>
+
+
+    </BrowserRouter>
   )
 }
 
