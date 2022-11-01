c03d06c1d4578c8a18a52560586ba5dc89486612
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-01 20:16:40 +0300

    seconad day

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+  const APP_ID = "80da54c7";
+  const APP_KEY = "c23a7f86fcdeddf5d8f0a3b60023f94c";
+  const [query, setQuery] = useState("");
+  const [selectedMeal, setSelectedMeal] = useState("");
+
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+
+  }
+
   return (
     <div>
       Home
