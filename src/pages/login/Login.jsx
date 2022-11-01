4e45c2f54d54f99968db3004fb1fdcee6ae97714
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-01 10:54:41 +0300

    login done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import LoginContainer from './Login.style'
+import { LoginContainer, FormContainer, StyledImg, Header, StyledInput, StyledForm, StyledButton } from './Login.style'
+import meal from "../../assets/meal.svg"
+import { useNavigate } from 'react-router-dom'
 
 
 const Login = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("-1")
+  }
+
   return (
     <LoginContainer>
-    
-    
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>
+          Shady's Receipe
+        </Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder='Enter User Name' type="text" />
+          <StyledInput placeholder='Enter Password' type="password" />
+          <StyledButton type='submit'>LOGIN
+          </StyledButton>
+
+        </StyledForm>
+
+      </FormContainer>
     </LoginContainer>
   )
 }
