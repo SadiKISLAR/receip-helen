4fe315154b22c61073cb5dbeef925cca65a44583
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-01 09:53:40 +0300

    privateroute

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { GlobalStyles } from '../components/globalStyles/GlobalStyles'
 import Navbar from "./../components/nav/Navbar"
 import Home from "../pages/home/Home"
+import Login from '../pages/login/Login'
+import About from '../pages/about/About'
+import Detail from '../pages/detail/Detail'
+import PrivateRouter from './PrivateRouter'
 
 const AppRouter = () => {
   return (
@@ -10,7 +14,16 @@ const AppRouter = () => {
       <GlobalStyles />
       <Navbar />
       <Routes>
-        <Route path="/" elemnet={<Home/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="login" element={<Login/>} />
+
+        <Route path="about" element={<PrivateRouter/>}>
+          <Route path='' element={<About/>}/>
+        </Route>
+        <Route path="details" element={<PrivateRouter/>}>
+          <Route path='' element={<Detail/>}/>
+        </Route>
+        
       </Routes>
 
 
